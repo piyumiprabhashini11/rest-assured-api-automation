0a7c496c831cac2b4059726cca39b76dcf304a72
piyumiprabhashini11 <piyumimadduamge1700@gmail.com>
2025-12-19 10:51:31 +0000

    Rest Assured tests

---
+++ src/test/java/DynamicJson.java
@@ -0,0 +1,24 @@
+import org.testng.annotations.Test;
+
+import files.Payload;
+
+import static io.restassured.RestAssured.*;
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
+public class DynamicJson {
+
+	@Test
+	public void addBook() {
+		RestAssured.baseURI="https://rahulshettyacademy.com";
+		JsonPath response=given().log().all().header("Content-Type","application/json").
+		body(Payload.AddBook()).
+		when().post("Library/Addbook.php").
+		then().log().all().assertThat().statusCode(200).
+		extract().response().jsonPath();	
+		String id=response.getString("ID");
+		System.out.println("ID= "+id);
+		
+	
+}
+}
